ccfda02a0ce7a9ecddc6f6b5a9635a3798e93e89
jerrypc <273412556@qq.com>
2024-05-14 04:03:53 +0800

    login page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

089c8de083b91a9a3ef1bf9cd73ada4f8b23c685
kanishk.uwa <23958847@student.uwa.edu.au>
2024-05-17 02:45:52 +0800

    Added new_game and game_screen pages.

---
+++ js/game_logic.js
@@ -0,0 +1,24 @@
+let playerText = document.getElementById('playerText');
+let boxes = Array.from(document.getElementsByClassName('box'));
+
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked(e) {
+    const id = e.target.id;
+
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
+}
+
+startGame()

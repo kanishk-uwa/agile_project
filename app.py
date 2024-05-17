ce21746238795ed36e4fb42fa8f5e6a0ac457c7e
smit2810 <smitkevadiyasdk@gmail.com>
2024-05-18 00:36:17 +0800

    Added My profile page and simple db file

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+def get_players():
+    conn = sqlite3.connect('players.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, ranking, matches_won, image_url FROM players ORDER BY ranking ASC")
+    players = cursor.fetchall()
+    conn.close()
+    return players
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    players = get_players()
+    return render_template('profile.html', players=players)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
